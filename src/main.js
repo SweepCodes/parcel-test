d4f6eca7049f51633e97520c05abcd35a157913b
Abdulla Jamal <93247639+SweepCodes@users.noreply.github.com>
2024-02-16 11:28:07 +0100

    animations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import sunFlowerImg from "./media/sf.jpg"
+import anime from "animejs";
 
 
 const img = document.createElement('img');
 document.body.append(img)
 img.src = sunFlowerImg;
+
+
+anime({
+    targets: img,
+    rotate: 360
+})
